e28b43abd287cb4e6742b431d7adbc941f0a729f
Stefan Theofilos <StefanTheofilos@Stefans-MacBook-Pro.local>
2016-05-09 10:11:41 -0500

    fixed login overlay

---
+++ app/index/controller.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  // constructor(login) {
+  //   this.login = login;
+  //   this.form.addEventListener(`submit`, (ev) => {
+  //     ev.preventDefault();
+  //     this.validateInputs();
+  //   });
+  // },
+});
