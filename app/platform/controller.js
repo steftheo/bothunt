32b73104b1f4848c299f74e5c408827137238341
Stefan Theofilos <StefanTheofilos@Stefans-MacBook-Pro.local>
2016-05-09 22:12:55 -0500

    save, edit, destroy platform and add-bot

--- app/platform/controller.js
+++ app/platform/controller.js
@@ -1,26 +1,57 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  savePlatform({ name, url }) {
-    const platform = this.store.createRecord(`platform`, {
-      name: this.name,
-      url: this.url,
-    });
+  actions: {
+    save(platform) {
+      this.get(`model`).set(`platform`, platform);
+      this.get(`model`).save().then(() => {
+        this.transitionToRoute(`index`);
+      });
+    },
 
-    platform.save().then(() => {
+    destroy() {
+      this.get(`model`).destroyRecord().then(() => {
+        this.transitionToRoute(`index`);
+      });
+    },
+
+    cancel() {
+      this.get(`model`).rollbackAttributes();
       this.transitionToRoute(`index`);
-    });
+    },
   },
 
-  updatePlatform() {
-    this.store.findRecord(`platform`, 1).then(function(link) {
-      link.set(this.url);
+  changedAttributesHash: Ember.computed(`model.{platform}`, function() {
+    let changedAttributes = this.get(`model`).changedAttributes();
+    let usableHash = Object.keys(changedAttributes).map(function(attributeName) {
+      return {
+        key: attributeName,
+        earlierValue: changedAttributes[attributeName][0],
+        currentBalue: changedAttributes[attributeName][1],
+      };
     });
-  },
+    return usableHash;
+  }),
+  // savePlatform({ name, url }) {
+  //   const platform = this.store.createRecord(`platform`, {
+  //     name: this.name,
+  //     url: this.url,
+  //   });
+  //
+  //   platform.save().then(() => {
+  //     this.transitionToRoute(`index`);
+  //   });
+  // },
 
-  destroyPlatform(platform) {
-    store.find(`platform`, 2).then(function (platform) {
-      platform.destroyRecord();
-    });
-  },
+  // updatePlatform() {
+  //   this.store.findRecord(`platform`, 1).then(function(link) {
+  //     link.set(this.url);
+  //   });
+  // },
+  //
+  // destroyPlatform(platform) {
+  //   store.find(`platform`, 2).then(function (platform) {
+  //     platform.destroyRecord();
+  //   });
+  // },
 });
