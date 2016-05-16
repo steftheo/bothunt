92eeea889f70c460bcb74946dddc0220f3197d93
Stefan Theofilos <StefanTheofilos@Stefans-MacBook-Pro.local>
2016-05-16 11:46:19 -0500

    Fixes login error

--- app/index/login/controller.js
+++ app/index/login/controller.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
 
-  login(email, password) {
+  login({ email, password }) {
     this.get(`session`).authenticate(`authenticator:application`, email, password)
       .then(() => {
         this.transitionToRoute(`index`);
