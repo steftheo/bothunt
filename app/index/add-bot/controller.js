e390cae7c39ec86599db23e10dbc736f52744115
Stefan Theofilos <StefanTheofilos@Stefans-MacBook-Pro.local>
2016-05-17 22:50:08 -0500

    added platform toggles

--- app/index/add-bot/controller.js
+++ app/index/add-bot/controller.js
@@ -1,11 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  saveBot(formValues) {
-    const bot = this.store.createRecord(`bot`, formValues);
+  saveBot({ name, description, imageUrl, websiteUrl, platforms }) {
+    const bot = this.store.createRecord(`bot`, { name, description, imageUrl, websiteUrl });
 
     bot.save().then(() => {
+      platforms.forEach((platform) => {
+        this.store.createRecord(`implementation`, { bot, platform }).save();
+      });
       this.transitionToRoute(`index`);
     });
   },
+
+  addPlatform(change, oldPlatforms, platform, isToggled) {
+    if (oldPlatforms === undefined) {
+      oldPlatforms = [];
+    }
+
+    if (isToggled) {
+      return change([...oldPlatforms, platform]);
+    }
+
+    change(oldPlatforms.filter(p => p !== platform));
+  }
 });
