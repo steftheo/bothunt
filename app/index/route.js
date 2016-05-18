2912e978143abba0cef10a8058eaeaab17f0b5fd
Stefan Theofilos <StefanTheofilos@Stefans-MacBook-Pro.local>
2016-05-17 20:59:42 -0500

    removed hardcoded platforms

--- app/index/route.js
+++ app/index/route.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   session: Ember.inject.service(),
 
-  model() {
-    return this.store.findAll(`bot`);
+  model: function() {
+    var store = this.store;
+    return Ember.RSVP.hash({
+      bots: store.findAll('bot'),
+      platforms: store.findAll('platform')
+    });
   },
 });
--- app/models/platform.js
+++ app/models/platform.js
@@ -4,6 +4,7 @@ import { hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   url: attr(),
+  logo: attr(),
   name: attr(),
   implementations: hasMany('implementation')
 });
