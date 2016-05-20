0f857a830217e7d9696ae16922b6cb4b1cc3040c
Ryan Tablada <ryan.tablada@gmail.com>
2016-05-19 22:28:10 -0500

    Check current user after login

--- app/application/route.js
+++ app/application/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  currentUser: Ember.inject.service(),
+
+  sessionAuthenticated() {
+    this._super(...arguments);
+    this.get(`currentUser`).checkCurrentUser();
+  }
+});
